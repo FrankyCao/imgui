fed25d26b92786b7a9d374de0e8db895ef01defd
Dicky <Dicky.Qi@icloud.com>
2021-02-23 19:39:59 +0800

    move wave plot show in other window

--- apps/MediaPlayer/media_player.cpp
+++ apps/MediaPlayer/media_player.cpp
@@ -33,7 +33,7 @@ int genpts = 0;
 int seek_by_bytes = -1;
 int autoexit = 0;
 int startup_volume = 100;
-int show_status = 1;
+int show_status = 0;
 int av_sync_type = AV_SYNC_AUDIO_MASTER;
 int64_t start_time = AV_NOPTS_VALUE;
 int64_t duration = AV_NOPTS_VALUE;
@@ -52,6 +52,7 @@ SDL_AudioDeviceID audio_dev;
 
 static VideoState * is = nullptr;
 static bool show_info = false;
+static bool show_wavefrom = false;
 
 static inline int compute_mod(int a, int b)
 {
@@ -154,90 +155,13 @@ bool Application_Frame(void* handle)
 
     // Show PlayControl panel
     ImVec2 center(io.DisplaySize.x * 0.5f, io.DisplaySize.y * 0.9f);
-    ImVec2 panel_size(io.DisplaySize.x - 20.0, 140);
+    ImVec2 panel_size(io.DisplaySize.x - 20.0, show_status ? 140 : 120);
     ImGui::SetNextWindowPos(center, ImGuiCond_Always, ImVec2(0.5f, 0.5f));
     ImGui::SetNextWindowSize(panel_size, ImGuiCond_Always);
     ImGui::SetNextWindowBgAlpha(0.5);
     if (ImGui::Begin("Control", NULL, ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoScrollbar | ImGuiWindowFlags_NoResize))
     {
         int i = ImGui::FindWindowByName("Control")->Size.x;
-        // add wave plots
-        if (is && is->audio_st)
-        {
-            int data_used = 128;
-            float left_wave[data_used], right_wave[data_used];
-            int64_t time_diff;
-            int i, x, h, i_start;
-            int channels = is->audio_tgt.channels;
-            if (!is->paused)
-            {
-                int n = 2 * channels;
-                int delay = is->audio_write_buf_size;
-                delay /= n;
-                if (audio_callback_time)
-                {
-                    time_diff = av_gettime_relative() - audio_callback_time;
-                    delay -= (time_diff * is->audio_tgt.freq) / 1000000;
-                }
-                delay += 2 * data_used;
-                if (delay < data_used)
-                    delay = data_used;
-                i_start = x = compute_mod(is->sample_array_index - delay * channels, SAMPLE_ARRAY_SIZE);
-                {
-                    h = INT_MIN;
-                    for (i = 0; i < 1000; i += channels) 
-                    {
-                        int idx = (SAMPLE_ARRAY_SIZE + x - i) % SAMPLE_ARRAY_SIZE;
-                        int a = is->sample_array[idx];
-                        int b = is->sample_array[(idx + 4 * channels) % SAMPLE_ARRAY_SIZE];
-                        int c = is->sample_array[(idx + 5 * channels) % SAMPLE_ARRAY_SIZE];
-                        int d = is->sample_array[(idx + 9 * channels) % SAMPLE_ARRAY_SIZE];
-                        int score = a - d;
-                        if (h < score && (b ^ c) < 0) 
-                        {
-                            h = score;
-                            i_start = idx;
-                        }
-                    }
-                    is->last_i_start = i_start;
-                }
-            }
-            else
-                i_start = is->last_i_start;
-            i = i_start;
-            for (x = 0; x < data_used; x++) 
-            {
-                float left_data = is->sample_array[i + 0] / 32768.f;
-                left_wave[x] = left_data;
-                if (channels > 1)
-                {
-                    float right_data = is->sample_array[i + 1] / 32768.f;
-                    right_wave[x] = right_data;
-                }
-                i += channels;
-                if (i >= SAMPLE_ARRAY_SIZE)
-                    i -= SAMPLE_ARRAY_SIZE;
-            }
-            ImPlotFlags flags = ImPlotFlags_CanvasOnly | ImPlotFlags_NoChild;
-            ImPlotAxisFlags axis_flags = ImPlotAxisFlags_NoDecorations | ImPlotAxisFlags_LockMin | ImPlotAxisFlags_LockMax;
-            ImPlot::PushStyleVar(ImPlotStyleVar_PlotBorderSize, 0.f);
-            ImPlot::PushStyleVar(ImPlotStyleVar_PlotPadding, ImVec2(0.f, 0.f));
-            ImPlot::SetNextPlotLimits(0, data_used, -1, 1);
-            if (ImPlot::BeginPlot("##Audio wave left", NULL, NULL, ImVec2(data_used,36), flags, axis_flags, axis_flags)) 
-            {
-                ImPlot::PlotLine("##lwave", left_wave, data_used);
-                ImPlot::EndPlot();
-            }
-            ImGui::SameLine();
-            ImPlot::SetNextPlotLimits(0, data_used, -1, 1);
-            if (ImPlot::BeginPlot("##Audio wave right", NULL, NULL, ImVec2(data_used,36), flags, axis_flags, axis_flags)) 
-            {
-                ImPlot::PlotLine("##rwave", right_wave, data_used);
-                ImPlot::EndPlot();
-            }
-            ImGui::SameLine();
-            ImPlot::PopStyleVar(2);
-        }
         // add button
         ImGui::Indent((i - 32.0f) * 0.5f);
         ImVec2 size = ImVec2(32.0f, 32.0f); // Size of the image we want to make visible
@@ -255,20 +179,26 @@ bool Application_Frame(void* handle)
         {
             if (is) show_info = !show_info;
         }
+        ImGui::SameLine();
+        if (ImGui::Button(ICON_FAD_WAVEFORM , size))
+        {
+            if (is) show_wavefrom = !show_wavefrom;
+        }
+        // add button end
 
         ImGui::Unindent((i - 32.0f) * 0.5f);
         ImGui::Separator();
         // add audio meter bar
         if (is)
         {
-            ImGui::UvMeter("##lhuvr", ImVec2(panel_size.x, 10), &is->audio_left_channel_level, 0, 90, 200); ImGui::ShowTooltipOnHover("Left Uv meters.");
-            ImGui::UvMeter("##rhuvr", ImVec2(panel_size.x, 10), &is->audio_right_channel_level, 0, 90, 200); ImGui::ShowTooltipOnHover("Right Uv meters.");
+            ImGui::UvMeter("##lhuvr", ImVec2(panel_size.x, 10), &is->audio_left_channel_level, 0, 90, 200);
+            ImGui::UvMeter("##rhuvr", ImVec2(panel_size.x, 10), &is->audio_right_channel_level, 0, 90, 200);
         }
         else
         {
             int zero_channel_level = 0;
-            ImGui::UvMeter("##lhuvr", ImVec2(panel_size.x, 10), &zero_channel_level, 0, 90, 200); ImGui::ShowTooltipOnHover("Left Uv meters.");
-            ImGui::UvMeter("##rhuvr", ImVec2(panel_size.x, 10), &zero_channel_level, 0, 90, 200); ImGui::ShowTooltipOnHover("Right Uv meters.");
+            ImGui::UvMeter("##lhuvr", ImVec2(panel_size.x, 10), &zero_channel_level, 0, 90, 200); // ImGui::ShowTooltipOnHover("Left Uv meters.");
+            ImGui::UvMeter("##rhuvr", ImVec2(panel_size.x, 10), &zero_channel_level, 0, 90, 200); // ImGui::ShowTooltipOnHover("Right Uv meters.");
         }
         ImGui::Separator();
         // add slider bar
@@ -312,12 +242,15 @@ bool Application_Frame(void* handle)
             float time = 0;
             ImGui::SliderFloat("time", &time, 0, 0, "%.2f", ImGuiSliderFlags_NoInput | ImGuiSliderFlags_NoLabel);
         }
-        ImGui::Separator();
-        // add stats bar
-        if (is && !is->stats_string.empty())
-            ImGui::Text("%s", is->stats_string.c_str());
-        else
-            ImGui::Text("no media");
+        if (show_status)
+        {
+            ImGui::Separator();
+            // add stats bar
+            if (is && !is->stats_string.empty())
+                ImGui::Text("%s", is->stats_string.c_str());
+            else
+                ImGui::Text("no media");
+        }
         ImGui::End();
     }
 
@@ -498,5 +431,110 @@ bool Application_Frame(void* handle)
         ImGui::End();
     }
 
+    // Show wavefrom
+    if (is && is->audio_st && show_wavefrom)
+    {
+        const float DISTANCE = 40.0f;
+        static int corner = 3;
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoSavedSettings | ImGuiWindowFlags_NoFocusOnAppearing | ImGuiWindowFlags_NoNav;
+        if (corner != -1)
+        {
+            window_flags |= ImGuiWindowFlags_NoMove;
+            ImVec2 window_pos = ImVec2((corner & 1) ? io.DisplaySize.x - DISTANCE : DISTANCE, (corner & 2) ? io.DisplaySize.y - DISTANCE : DISTANCE);
+            ImVec2 window_pos_pivot = ImVec2((corner & 1) ? 1.0f : 0.0f, (corner & 2) ? 1.0f : 0.0f);
+            ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always, window_pos_pivot);
+        }
+        ImGui::SetNextWindowBgAlpha(0.35f); // Transparent background
+        if (ImGui::Begin("Wave Plot", nullptr, window_flags))
+        {
+            int data_used = 256;
+            int view_height = data_used / 4;
+            float left_wave[data_used], right_wave[data_used];
+            int64_t time_diff;
+            int i, x, h, i_start;
+            int channels = is->audio_tgt.channels;
+            if (!is->paused)
+            {
+                int n = 2 * channels;
+                int delay = is->audio_write_buf_size;
+                delay /= n;
+                if (audio_callback_time)
+                {
+                    time_diff = av_gettime_relative() - audio_callback_time;
+                    delay -= (time_diff * is->audio_tgt.freq) / 1000000;
+                }
+                delay += 2 * data_used;
+                if (delay < data_used)
+                    delay = data_used;
+                i_start = x = compute_mod(is->sample_array_index - delay * channels, SAMPLE_ARRAY_SIZE);
+                {
+                    h = INT_MIN;
+                    for (i = 0; i < 1000; i += channels) 
+                    {
+                        int idx = (SAMPLE_ARRAY_SIZE + x - i) % SAMPLE_ARRAY_SIZE;
+                        int a = is->sample_array[idx];
+                        int b = is->sample_array[(idx + 4 * channels) % SAMPLE_ARRAY_SIZE];
+                        int c = is->sample_array[(idx + 5 * channels) % SAMPLE_ARRAY_SIZE];
+                        int d = is->sample_array[(idx + 9 * channels) % SAMPLE_ARRAY_SIZE];
+                        int score = a - d;
+                        if (h < score && (b ^ c) < 0) 
+                        {
+                            h = score;
+                            i_start = idx;
+                        }
+                    }
+                    is->last_i_start = i_start;
+                }
+            }
+            else
+                i_start = is->last_i_start;
+            i = i_start;
+            for (x = 0; x < data_used; x++) 
+            {
+                float left_data = is->sample_array[i + 0] / 32768.f;
+                left_wave[x] = left_data;
+                if (channels > 1)
+                {
+                    float right_data = is->sample_array[i + 1] / 32768.f;
+                    right_wave[x] = right_data;
+                }
+                i += channels;
+                if (i >= SAMPLE_ARRAY_SIZE)
+                    i -= SAMPLE_ARRAY_SIZE;
+            }
+            ImPlotStyle& style = ImPlot::GetStyle();
+            style.Colors[ImPlotCol_PlotBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+            style.Colors[ImPlotCol_FrameBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.20f);
+            style.Colors[ImPlotCol_Line] = ImVec4(0.00f, 1.00f, 0.00f, 1.00f);
+            ImPlotFlags flags = ImPlotFlags_CanvasOnly | ImPlotFlags_NoChild;
+            ImPlotAxisFlags axis_flags = ImPlotAxisFlags_NoDecorations | ImPlotAxisFlags_LockMin | ImPlotAxisFlags_LockMax;
+            ImPlot::PushStyleVar(ImPlotStyleVar_PlotBorderSize, 0.f);
+            ImPlot::PushStyleVar(ImPlotStyleVar_PlotPadding, ImVec2(0.f, 0.f));
+            ImPlot::SetNextPlotLimits(0, data_used, -1, 1);
+            if (ImPlot::BeginPlot("##Audio wave left", NULL, NULL, ImVec2(data_used, view_height), flags, axis_flags, axis_flags)) 
+            {
+                ImPlot::PlotLine("##lwave", left_wave, data_used);
+                ImPlot::EndPlot();
+            }
+            ImGui::Separator();
+            ImPlot::SetNextPlotLimits(0, data_used, -1, 1);
+            if (ImPlot::BeginPlot("##Audio wave right", NULL, NULL, ImVec2(data_used, view_height), flags, axis_flags, axis_flags)) 
+            {
+                ImPlot::PlotLine("##rwave", right_wave, data_used);
+                ImPlot::EndPlot();
+            }
+            ImPlot::PopStyleVar(2);
+        }
+        if (ImGui::BeginPopupContextWindow())
+        {
+            if (ImGui::MenuItem("Custom",       NULL, corner == -1)) corner = -1;
+            if (ImGui::MenuItem("Top-left",     NULL, corner == 0)) corner = 0;
+            if (ImGui::MenuItem("Top-right",    NULL, corner == 1)) corner = 1;
+            if (ImGui::MenuItem("Bottom-left",  NULL, corner == 2)) corner = 2;
+            if (ImGui::MenuItem("Bottom-right", NULL, corner == 3)) corner = 3;
+            ImGui::EndPopup();
+        }
+        ImGui::End();
+    }
     return done;
 }
