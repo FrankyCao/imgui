7586eff3b77b629d8467acfb64acd26c4b598332
Dicky <Dicky.Qi@icloud.com>
2021-03-15 20:27:52 +0800

    1. update imgui 2. move blueprint demo imex functions to imgui core

--- addon/ImGuiHelper/ImGuiHelper.cpp
+++ addon/ImGuiHelper/ImGuiHelper.cpp
@@ -6,6 +6,7 @@
 //-----------------------------------------------------------------------------------------------------------------
 
 #include "ImGuiHelper.h"
+#include <errno.h>
 
 #ifdef _WIN32
 #include <windows.h>
@@ -618,6 +619,13 @@ bool IsItemJustReleased()   {
     return IsItemActiveLastFrame() && !ImGui::IsItemActive();
 }
 
+void Debug_DrawItemRect(const ImVec4& col)
+{
+    auto drawList = ImGui::GetWindowDrawList();
+    auto itemMin = ImGui::GetItemRectMin();
+    auto itemMax = ImGui::GetItemRectMax();
+    drawList->AddRect(itemMin, itemMax, ImColor(col));
+}
 
 #ifndef NO_IMGUIHELPER_FONT_METHODS
 const ImFont *GetFont(int fntIndex) {return (fntIndex>=0 && fntIndex<ImGui::GetIO().Fonts->Fonts.size()) ? ImGui::GetIO().Fonts->Fonts[fntIndex] : NULL;}
@@ -1101,11 +1109,245 @@ void PutInForeground(const char* optionalRootWindowName)  {
     }
 }
 
+ScopedItemWidth::ScopedItemWidth(float width)
+{
+    ImGui::PushItemWidth(width);
+}
+
+ScopedItemWidth::~ScopedItemWidth()
+{
+    Release();
+}
+
+void ScopedItemWidth::Release()
+{
+    if (m_IsDone)
+        return;
+
+    ImGui::PopItemWidth();
+
+    m_IsDone = true;
+}
+
+ScopedDisableItem::ScopedDisableItem(bool disable, float disabledAlpha)
+    : m_Disable(disable)
+{
+    if (!m_Disable)
+        return;
+
+    auto wasDisabled = (ImGui::GetCurrentWindow()->DC.ItemFlags & ImGuiItemFlags_Disabled) == ImGuiItemFlags_Disabled;
+
+    ImGui::PushItemFlag(ImGuiItemFlags_Disabled, true);
+
+    auto& stale = ImGui::GetStyle();
+    m_LastAlpha = stale.Alpha;
+
+    // Don't override alpha if we're already in disabled context.
+    if (!wasDisabled)
+        stale.Alpha = disabledAlpha;
+}
+
+ScopedDisableItem::~ScopedDisableItem()
+{
+    Release();
+}
+
+void ScopedDisableItem::Release()
+{
+    if (!m_Disable)
+        return;
+
+    auto& stale = ImGui::GetStyle();
+    stale.Alpha = m_LastAlpha;
+
+    ImGui::PopItemFlag();
+
+    m_Disable = false;
+}
+
+ScopedSuspendLayout::ScopedSuspendLayout()
+{
+    m_Window = ImGui::GetCurrentWindow();
+    m_CursorPos = m_Window->DC.CursorPos;
+    m_CursorPosPrevLine = m_Window->DC.CursorPosPrevLine;
+    m_CursorMaxPos = m_Window->DC.CursorMaxPos;
+    m_CurrLineSize = m_Window->DC.CurrLineSize;
+    m_PrevLineSize = m_Window->DC.PrevLineSize;
+    m_CurrLineTextBaseOffset = m_Window->DC.CurrLineTextBaseOffset;
+    m_PrevLineTextBaseOffset = m_Window->DC.PrevLineTextBaseOffset;
+}
+
+ScopedSuspendLayout::~ScopedSuspendLayout()
+{
+    Release();
+}
+
+void ScopedSuspendLayout::Release()
+{
+    if (m_Window == nullptr)
+        return;
+
+    m_Window->DC.CursorPos = m_CursorPos;
+    m_Window->DC.CursorPosPrevLine = m_CursorPosPrevLine;
+    m_Window->DC.CursorMaxPos = m_CursorMaxPos;
+    m_Window->DC.CurrLineSize = m_CurrLineSize;
+    m_Window->DC.PrevLineSize = m_PrevLineSize;
+    m_Window->DC.CurrLineTextBaseOffset = m_CurrLineTextBaseOffset;
+    m_Window->DC.PrevLineTextBaseOffset = m_PrevLineTextBaseOffset;
+
+    m_Window = nullptr;
+}
+
+ItemBackgroundRenderer::ItemBackgroundRenderer(OnDrawCallback onDrawBackground)
+    : m_OnDrawBackground(std::move(onDrawBackground))
+{
+    m_DrawList = ImGui::GetWindowDrawList();
+    m_Splitter.Split(m_DrawList, 2);
+    m_Splitter.SetCurrentChannel(m_DrawList, 1);
+}
+
+ItemBackgroundRenderer::~ItemBackgroundRenderer()
+{
+    Commit();
+}
+
+void ItemBackgroundRenderer::Commit()
+{
+    if (m_Splitter._Current == 0)
+        return;
+
+    m_Splitter.SetCurrentChannel(m_DrawList, 0);
+
+    if (m_OnDrawBackground)
+        m_OnDrawBackground(m_DrawList);
+
+    m_Splitter.Merge(m_DrawList);
+}
+
+void ItemBackgroundRenderer::Discard()
+{
+    if (m_Splitter._Current == 1)
+        m_Splitter.Merge(m_DrawList);
+}
+
+StorageHandler<MostRecentlyUsedList::Settings> MostRecentlyUsedList::s_Storage;
 
 
+void MostRecentlyUsedList::Install(ImGuiContext* context)
+{
+    context->SettingsHandlers.push_back(s_Storage.MakeHandler("MostRecentlyUsedList"));
+
+    s_Storage.ReadLine = [](ImGuiContext*, Settings* entry, const char* line)
+    {
+        const char* lineEnd = line + strlen(line);
+
+        auto parseListEntry = [lineEnd](const char* line, int& index) -> const char*
+        {
+            char* indexEnd = nullptr;
+            errno = 0;
+            index = strtol(line, &indexEnd, 10);
+            if (errno == ERANGE)
+                return nullptr;
+            if (indexEnd >= lineEnd)
+                return nullptr;
+            if (*indexEnd != '=')
+                return nullptr;
+            return indexEnd + 1;
+        };
+
+
+        int index = 0;
+        if (auto path = parseListEntry(line, index))
+        {
+            if (static_cast<int>(entry->m_List.size()) <= index)
+                entry->m_List.resize(index + 1);
+            entry->m_List[index] = path;
+        }
+    };
+
+    s_Storage.WriteAll = [](ImGuiContext*, ImGuiTextBuffer* out_buf, const StorageHandler<Settings>::Storage& storage)
+    {
+        for (auto& entry : storage)
+        {
+            out_buf->appendf("[%s][%s]\n", "MostRecentlyUsedList", entry.first.c_str());
+            int index = 0;
+            for (auto& value : entry.second->m_List)
+                out_buf->appendf("%d=%s\n", index++, value.c_str());
+            out_buf->append("\n");
+        }
+    };
+}
+
+MostRecentlyUsedList::MostRecentlyUsedList(const char* id, int capacity /*= 10*/)
+    : m_ID(id)
+    , m_Capacity(capacity)
+    , m_List(s_Storage.FindOrCreate(id)->m_List)
+{
+}
+
+void MostRecentlyUsedList::Add(const std::string& item)
+{
+    Add(item.c_str());
+}
+
+void MostRecentlyUsedList::Add(const char* item)
+{
+    auto itemIt = std::find(m_List.begin(), m_List.end(), item);
+    if (itemIt != m_List.end())
+    {
+        // Item is already on the list. Rotate list to move it to the
+        // first place.
+        std::rotate(m_List.begin(), itemIt, itemIt + 1);
+    }
+    else
+    {
+        // Push new item to the back, rotate list to move it to the front,
+        // pop back last element if we're over capacity.
+        m_List.push_back(item);
+        std::rotate(m_List.begin(), m_List.end() - 1, m_List.end());
+        if (static_cast<int>(m_List.size()) > m_Capacity)
+            m_List.pop_back();
+    }
+
+    PushToStorage();
+
+    ImGui::MarkIniSettingsDirty();
+}
+
+void MostRecentlyUsedList::Clear()
+{
+    if (m_List.empty())
+        return;
+
+    m_List.resize(0);
+
+    PushToStorage();
+
+    ImGui::MarkIniSettingsDirty();
+}
+
+const std::vector<std::string>& MostRecentlyUsedList::GetList() const
+{
+    return m_List;
+}
+
+int MostRecentlyUsedList::Size() const
+{
+    return static_cast<int>(m_List.size());
+}
+
+void MostRecentlyUsedList::PullFromStorage()
+{
+    if (auto settings = s_Storage.Find(m_ID.c_str()))
+        m_List = settings->m_List;
+}
+
+void MostRecentlyUsedList::PushToStorage()
+{
+    auto settings = s_Storage.FindOrCreate(m_ID.c_str());
+    settings->m_List = m_List;
+}
 } // namespace Imgui
-
-
 
 #ifndef NO_IMGUIHELPER_SERIALIZATION
 #include <stdio.h>  // FILE
--- addon/ImGuiHelper/ImGuiHelper.h
+++ addon/ImGuiHelper/ImGuiHelper.h
@@ -1,12 +1,16 @@
 #ifndef IMGUIHELPER_H_
 #define IMGUIHELPER_H_
 
-#ifndef IMGUI_API
 #include <imgui.h>
-#endif //IMGUI_API
+#define IMGUI_DEFINE_MATH_OPERATORS
+#include <imgui_internal.h>
 #ifdef IMGUI_VULKAN_SHADER
 #include "gpu.h"
 #endif
+#include <functional>
+#include <map>
+#include <string>
+#include <vector>
 
 namespace ImGui {
 // ImGui Info
@@ -30,6 +34,8 @@ IMGUI_API void CloseAllPopupMenus();  // Never Tested
 IMGUI_API bool IsItemActiveLastFrame();
 IMGUI_API bool IsItemJustReleased();
 
+// Drawn an rectangle around last ImGui widget.
+IMGUI_API void Debug_DrawItemRect(const ImVec4& col = ImVec4(1.0f, 0.0f, 0.0f, 1.0f));
 
 #ifndef NO_IMGUIHELPER_FONT_METHODS
 IMGUI_API const ImFont* GetFont(int fntIndex);
@@ -76,7 +82,6 @@ IMGUI_API void ImDrawListAddRectWithHorizontalGradient(ImDrawList *dl, const ImV
 // PutInForeground() is better then ImGui::SetWindowFocus()
 IMGUI_API void PutInBackground(const char* optionalRootWindowName=NULL);
 IMGUI_API void PutInForeground(const char* optionalRootWindowName=NULL);
-
 
 #   ifdef IMGUI_USE_ZLIB	// requires linking to library -lZlib
 // Two methods that fill rv and return true on success
@@ -139,6 +144,173 @@ enum FieldType {
     FT_COUNT
 };
 
+struct ScopedItemWidth
+{
+    ScopedItemWidth(float width);
+    ~ScopedItemWidth();
+
+    void Release();
+
+private:
+    bool m_IsDone = false;
+};
+
+struct ScopedDisableItem
+{
+    ScopedDisableItem(bool disable, float disabledAlpha = 0.5f);
+    ~ScopedDisableItem();
+
+    void Release();
+
+private:
+    bool    m_Disable       = false;
+    float   m_LastAlpha     = 1.0f;
+};
+
+struct ScopedSuspendLayout
+{
+    ScopedSuspendLayout();
+    ~ScopedSuspendLayout();
+
+    void Release();
+
+private:
+    ImGuiWindow* m_Window = nullptr;
+    ImVec2 m_CursorPos;
+    ImVec2 m_CursorPosPrevLine;
+    ImVec2 m_CursorMaxPos;
+    ImVec2 m_CurrLineSize;
+    ImVec2 m_PrevLineSize;
+    float  m_CurrLineTextBaseOffset;
+    float  m_PrevLineTextBaseOffset;
+};
+
+struct ItemBackgroundRenderer
+{
+    using OnDrawCallback = std::function<void(ImDrawList* drawList)>;
+
+    ItemBackgroundRenderer(OnDrawCallback onDrawBackground);
+    ~ItemBackgroundRenderer();
+
+    void Commit();
+    void Discard();
+
+private:
+    ImDrawList*         m_DrawList = nullptr;
+    ImDrawListSplitter  m_Splitter;
+    OnDrawCallback      m_OnDrawBackground;
+};
+
+template <typename Settings>
+struct StorageHandler
+{
+    using Storage = std::map<std::string, std::unique_ptr<Settings>>;
+
+    ImGuiSettingsHandler MakeHandler(const char* const typeName)
+    {
+        ImGuiSettingsHandler handler;
+        handler.TypeName = typeName;
+        handler.TypeHash = ImHashStr(typeName);
+        handler.UserData = this;
+        handler.ReadOpenFn = [](ImGuiContext* ctx, ImGuiSettingsHandler* handler, const char* name) -> void*
+        {
+            auto storage = reinterpret_cast<StorageHandler*>(handler->UserData);
+            return storage->DoReadOpen(ctx, name);
+        };
+        handler.ReadLineFn = [](ImGuiContext* ctx, ImGuiSettingsHandler* handler, void* entry, const char* line) -> void
+        {
+            auto storage = reinterpret_cast<StorageHandler*>(handler->UserData);
+            storage->DoReadLine(ctx, reinterpret_cast<Settings*>(entry), line);
+        };
+        handler.WriteAllFn = [](ImGuiContext* ctx, ImGuiSettingsHandler* handler, ImGuiTextBuffer* out_buf)
+        {
+            auto storage = reinterpret_cast<StorageHandler*>(handler->UserData);
+            storage->DoWriteAll(ctx, out_buf);
+        };
+        return handler;
+    }
+
+    const Settings* Find(const char* name) const
+    {
+        auto it = m_Settings.find(name);
+        if (it == m_Settings.end())
+            return nullptr;
+
+        return it->second.get();
+    }
+
+    Settings* Find(const char* name)
+    {
+        return const_cast<Settings*>(const_cast<const StorageHandler*>(this)->Find(name));
+    }
+
+    Settings* FindOrCreate(const char* name)
+    {
+        auto settings = Find(name);
+        if (settings == nullptr)
+        {
+            auto it = m_Settings.emplace(name, std::make_unique<Settings>());
+            settings = it.first->second.get();
+        }
+        return settings;
+    }
+
+    std::function<void(ImGuiContext*, Settings*, const char*)>              ReadOpen;
+    std::function<void(ImGuiContext*, Settings*, const char*)>              ReadLine;
+    std::function<void(ImGuiContext*, ImGuiTextBuffer*, const Storage&)>    WriteAll;
+
+private:
+    Settings* DoReadOpen(ImGuiContext* ctx, const char* name)
+    {
+        auto settings = FindOrCreate(name);
+        if (ReadOpen)
+            ReadOpen(ctx, settings, name);
+        return settings;
+    }
+
+    void DoReadLine(ImGuiContext* ctx, Settings* entry, const char* line)
+    {
+        if (ReadLine)
+            ReadLine(ctx, entry, line);
+    }
+
+    void DoWriteAll(ImGuiContext* ctx, ImGuiTextBuffer* out_buf)
+    {
+        if (WriteAll)
+            WriteAll(ctx, out_buf, m_Settings);
+    }
+
+    std::map<std::string, std::unique_ptr<Settings>> m_Settings;
+};
+struct MostRecentlyUsedList
+{
+    static void Install(ImGuiContext* context);
+
+    MostRecentlyUsedList(const char* id, int capacity = 10);
+
+    void Add(const std::string& item);
+    void Add(const char* item);
+    void Clear();
+
+    const std::vector<std::string>& GetList() const;
+
+    int Size() const;
+
+private:
+    struct Settings
+    {
+        std::vector<std::string> m_List;
+    };
+
+    void PullFromStorage();
+    void PushToStorage();
+
+    std::string                 m_ID;
+    int                         m_Capacity = 10;
+    std::vector<std::string>&   m_List;
+
+    static StorageHandler<Settings> s_Storage;
+};
 }   // ImGui
 
 
