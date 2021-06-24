41952646247ab4b182188fb64b3e016cb8df3401
Dicky <Dicky.Qi@icloud.com>
2021-06-24 12:29:36 +0800

    1. update glslang to 11.5.0 2. sync imgui 3. small format modify

--- misc/cpp/dir_iterate.cpp
+++ misc/cpp/dir_iterate.cpp
@@ -10,45 +10,43 @@
 #include <string>
 #include <algorithm>
 
-using namespace std;
-
-std::vector<std::string> split(std::string str,std::string pattern)
+static std::vector<std::string> split(std::string str, std::string pattern)
 {
     std::string::size_type pos;
     std::vector<std::string> result;
-    str+=pattern;//扩展字符串以方便操作
+    str += pattern;//扩展字符串以方便操作
     int size=str.size();
 
-    for(int i=0; i<size; i++)
+    for(int i = 0; i < size; i++)
     {
-        pos=str.find(pattern,i);
-        if(pos<size)
+        pos = str.find(pattern, i);
+        if(pos < size)
         {
-            std::string s=str.substr(i,pos-i);
+            std::string s = str.substr(i, pos - i);
             result.push_back(s);
-            i=pos+pattern.size()-1;
+            i = pos + pattern.size() - 1;
         }
     }
     return result;
 }
 
-int DIR_Iterate(string directory, vector<string>& filesAbsolutePath, vector<string>& filesname, bool surfix, string filters)
+int DIR_Iterate(std::string directory, std::vector<std::string>& filesAbsolutePath, std::vector<std::string>& filesname, bool surfix, std::string filters)
 {
     DIR* dir = opendir(directory.c_str());
     if ( dir == NULL )
     {
-        cout<<directory<<" is not a directory or not exist!"<<endl;
+        std::cout << directory << " is not a directory or not exist!" << std::endl;
         return -1;
     }
+    
     struct dirent* d_ent = NULL;
     char dot[3] = ".";
     char dotdot[6] = "..";
-    std::vector<string> filter_array;
+    std::vector<std::string> filter_array;
     if (!filters.empty())
     {
         filter_array = split(filters, "|");
     }
-    
     while ( (d_ent = readdir(dir)) != NULL )
     {
         if ( (strcmp(d_ent->d_name, dot) != 0)
@@ -56,10 +54,10 @@ int DIR_Iterate(string directory, vector<string>& filesAbsolutePath, vector<stri
         {
             if ( d_ent->d_type == DT_DIR )
             {
-                string newDirectory = directory + string("/") + string(d_ent->d_name);
+                std::string newDirectory = directory + std::string("/") + std::string(d_ent->d_name);
                 if( directory[directory.length()-1] == '/')
                 {
-                    newDirectory = directory + string(d_ent->d_name);
+                    newDirectory = directory + std::string(d_ent->d_name);
                 }
                 
                 if ( -1 == DIR_Iterate(newDirectory, filesAbsolutePath, filesname, surfix, filters) )
@@ -71,13 +69,13 @@ int DIR_Iterate(string directory, vector<string>& filesAbsolutePath, vector<stri
             {
                 if (d_ent->d_name[0] == '.')
                 continue;
-                string absolutePath = directory + string("/") + string(d_ent->d_name);
+                std::string absolutePath = directory + std::string("/") + std::string(d_ent->d_name);
                 if( directory[directory.length()-1] == '/')
                 {
-                    absolutePath = directory + string(d_ent->d_name);
+                    absolutePath = directory + std::string(d_ent->d_name);
                 }
                 char * pos = strchr(d_ent->d_name, '.');
-                string surfix_name = string(pos);
+                std::string surfix_name = std::string(pos);
                 if (!surfix)
                 {
                     *pos = '\0';
