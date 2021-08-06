96adb32f49ba6300dcaa62ae42790c29e8c07c9e
Dicky <Dicky.Qi@icloud.com>
2021-08-06 12:28:57 +0800

    move colorconvert/resize vulkan class into filters folder

--- addon/ImVulkanShader/ImVulkanShader.h
+++ addon/ImVulkanShader/ImVulkanShader.h
@@ -3,8 +3,8 @@
 #include "gpu.h"
 #include "pipeline.h"
 #include "command.h"
-#include "ColorConvert_vulkan.h"
-#include "Resize_vulkan.h"
+#include "filters/ColorConvert_vulkan.h"
+#include "filters/Resize_vulkan.h"
 #include <vulkan/vulkan.h>
 
 namespace ImGui
