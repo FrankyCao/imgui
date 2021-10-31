bb7289e850429a29b111726b1f9c088b270afbb2
Dicky <Dicky.Qi@icloud.com>
2021-10-31 21:16:06 +0800

    move ALM filter from test

---
+++ addon/ImVulkanShader/filters/ALM_vulkan.cpp
@@ -0,0 +1,146 @@
+#include "ALM_vulkan.h"
+#include "ALM_shader.h"
+#include "ImVulkanShader.h"
+
+namespace ImGui 
+{
+ALM_vulkan::ALM_vulkan(int gpu)
+{
+    vkdev = ImGui::get_gpu_device(gpu);
+    opt.blob_vkallocator = vkdev->acquire_blob_allocator();
+    opt.staging_vkallocator = vkdev->acquire_staging_allocator();
+    opt.use_image_storage = false;
+    opt.use_fp16_arithmetic = true;
+    opt.use_fp16_storage = true;
+    cmd = new ImGui::VkCompute(vkdev);
+    std::vector<ImGui::vk_specialization_type> specializations(0);
+    std::vector<uint32_t> spirv_data;
+
+    ImGui::compile_spirv_module(ALM_data, opt, spirv_data);
+    pipe = new ImGui::Pipeline(vkdev);
+    pipe->set_optimal_local_size_xyz(16, 16, 1);
+    pipe->create(spirv_data.data(), spirv_data.size() * 4, specializations);
+
+    cmd->reset();
+}
+
+ALM_vulkan::~ALM_vulkan()
+{
+    if (vkdev)
+    {
+        if (pipe) { delete pipe; pipe = nullptr; }
+        if (cmd) { delete cmd; cmd = nullptr; }
+        if (opt.blob_vkallocator) { vkdev->reclaim_blob_allocator(opt.blob_vkallocator); opt.blob_vkallocator = nullptr; }
+        if (opt.staging_vkallocator) { vkdev->reclaim_staging_allocator(opt.staging_vkallocator); opt.staging_vkallocator = nullptr; }
+    }
+}
+
+void ALM_vulkan::SetParam(float _strength, float _bias, float _gamma)
+{
+    strength = (_strength >= 0  && _strength <= 4.0) ? _strength : 0.5;
+    bias = _bias;
+    gamma = _gamma;
+}
+
+void ALM_vulkan::upload_param(const ImGui::VkMat& src, ImGui::VkMat& dst)
+{
+    std::vector<ImGui::VkMat> bindings(8);
+    if      (dst.type == IM_DT_INT8)     bindings[0] = dst;
+    else if (dst.type == IM_DT_INT16)    bindings[1] = dst;
+    else if (dst.type == IM_DT_FLOAT16)  bindings[2] = dst;
+    else if (dst.type == IM_DT_FLOAT32)  bindings[3] = dst;
+
+    if      (src.type == IM_DT_INT8)     bindings[4] = src;
+    else if (src.type == IM_DT_INT16)    bindings[5] = src;
+    else if (src.type == IM_DT_FLOAT16)  bindings[6] = src;
+    else if (src.type == IM_DT_FLOAT32)  bindings[7] = src;
+
+    std::vector<ImGui::vk_constant_type> constants(13);
+    constants[0].i = src.w;
+    constants[1].i = src.h;
+    constants[2].i = src.c;
+    constants[3].i = src.color_format;
+    constants[4].i = src.type;
+    constants[5].i = dst.w;
+    constants[6].i = dst.h;
+    constants[7].i = dst.c;
+    constants[8].i = dst.color_format;
+    constants[9].i = dst.type;
+    constants[10].f = strength;
+    constants[11].f = bias;
+    constants[12].f = gamma;
+    cmd->record_pipeline(pipe, bindings, constants, dst);
+}
+
+void ALM_vulkan::filter(const ImGui::ImMat& src, ImGui::ImMat& dst)
+{
+    if (!vkdev || !pipe || !cmd)
+    {
+        return;
+    }
+    dst.create_type(src.w, src.h, 4, dst.type);
+
+    ImGui::VkMat out_gpu;
+    out_gpu.create_like(dst, opt.blob_vkallocator);
+    ImGui::VkMat in_gpu;
+    cmd->record_clone(src, in_gpu, opt);
+
+    upload_param(in_gpu, out_gpu);
+
+    // download
+    cmd->record_clone(out_gpu, dst, opt);
+    cmd->submit_and_wait();
+    cmd->reset();
+}
+
+void ALM_vulkan::filter(const ImGui::ImMat& src, ImGui::VkMat& dst)
+{
+    if (!vkdev || !pipe  || !cmd)
+    {
+        return;
+    }
+
+    dst.create_type(src.w, src.h, 4, dst.type, opt.blob_vkallocator);
+
+    ImGui::VkMat in_gpu;
+    cmd->record_clone(src, in_gpu, opt);
+
+    upload_param(in_gpu, dst);
+
+    cmd->submit_and_wait();
+    cmd->reset();
+}
+
+void ALM_vulkan::filter(const ImGui::VkMat& src, ImGui::ImMat& dst)
+{
+    if (!vkdev || !pipe || !cmd)
+    {
+        return;
+    }
+    dst.create_type(src.w, src.h, 4, dst.type);
+
+    ImGui::VkMat out_gpu;
+    out_gpu.create_like(dst, opt.blob_vkallocator);
+
+    upload_param(src, out_gpu);
+
+    // download
+    cmd->record_clone(out_gpu, dst, opt);
+    cmd->submit_and_wait();
+    cmd->reset();
+}
+
+void ALM_vulkan::filter(const ImGui::VkMat& src, ImGui::VkMat& dst)
+{
+    if (!vkdev || !pipe || !cmd)
+    {
+        return;
+    }
+    dst.create_type(src.w, src.h, 4, dst.type, opt.blob_vkallocator);
+
+    upload_param(src, dst);
+
+    cmd->submit_and_wait();
+    cmd->reset();
+}
+} // namespace ImGui 
