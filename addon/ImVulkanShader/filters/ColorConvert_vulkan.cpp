82d321b45b6a57d6021748a4622d49b5aa2b52d6
Dicky <Dicky.Qi@icloud.com>
2022-01-27 17:08:59 +0800

    disable color convert vulkan shader using fp16, it has accuracy issue for int16 convert

--- addon/ImVulkanShader/filters/ColorConvert_vulkan.cpp
+++ addon/ImVulkanShader/filters/ColorConvert_vulkan.cpp
@@ -13,7 +13,7 @@ ColorConvert_vulkan::ColorConvert_vulkan(int gpu)
     opt.staging_vkallocator = vkdev->acquire_staging_allocator();
     opt.use_image_storage = true;
     opt.use_fp16_arithmetic = true;
-    opt.use_fp16_storage = true;
+    opt.use_fp16_storage = false;   // fp16 has accuracy issue for int16 convert
     cmd = new VkCompute(vkdev);
     std::vector<vk_specialization_type> specializations(0);
     std::vector<uint32_t> spirv_data;
